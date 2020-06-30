2d7f28b0817df251052d95db1e268c8c81dc0c79
Melissa B <missmelissaboots@gmail.com>
2020-06-30 11:06:42 -0400

    verify not working

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        static private List<User> Users = new List<User>();
+        public IActionResult Index()
+        {
+            ViewBag.users = Users;
+            return View();
+        }
+        
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if(newUser.Password == verify)
+            {
+                ViewBag.user = newUser;
+                //Users.Add(newUser);
+                return View("Index");
+            }
+            else
+            {
+                return View("Add");
+            }
+        }
+    }
+}
